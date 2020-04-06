be80f79b7426d4696eaf680332179aeae70a106c
AlAnsarr <gocer.efekan@gmail.com>
2020-04-06 18:57:15 +0200

    avancer avec Z et reculer avec S

--- SceneKeyAdapter.java
+++ SceneKeyAdapter.java
@@ -1,73 +1,94 @@
+package SubmarineSimulator;
+
+
 import com.jogamp.newt.event.KeyAdapter;
-import com.jogamp.newt.event.KeyEvent; 
+import com.jogamp.newt.event.KeyEvent;
 
 
 class SceneKeyAdapter extends KeyAdapter {
-    
-	private float view_rotx, view_roty;
-	private float zoom = 1;
-	
-	private MyGLEventListener myGLEventListener;
 
-	
-	public SceneKeyAdapter (MyGLEventListener _myGLEventListener) {
-		myGLEventListener = _myGLEventListener;
-		view_rotx = _myGLEventListener.getView_rotx();
-		view_roty = _myGLEventListener.getView_roty();
-	}
+    private float view_rotx, view_roty;
+    private float zoom = 1;
+
+    private MyGLEventListener myGLEventListener;
 
 
-	@Override
-	public void keyPressed(KeyEvent e) {
+    public SceneKeyAdapter (MyGLEventListener _myGLEventListener) {
+        myGLEventListener = _myGLEventListener;
+        view_rotx = _myGLEventListener.getView_rotx();
+        view_roty = _myGLEventListener.getView_roty();
+    }
+
+
+    @Override
+    public void keyPressed(KeyEvent e) {
         int kc = e.getKeyCode();
+        System.out.println("key : " + e.getKeyCode());
 
         view_rotx = myGLEventListener.getView_rotx();
-		view_roty = myGLEventListener.getView_roty();
-		
+        view_roty = myGLEventListener.getView_roty();
+
+//        Ctrl = 17 -> descendre
+//        Maj = 15
+//        Z = 90
+//        S = 83
+//        Q = 81
+//        D = 68
+
+        if(83 == kc) {
+            //S est tapé, on recule
+            myGLEventListener.reculer();
+        }
+
+        if(90 == kc) {
+            //Z est tapé, on avance
+            myGLEventListener.avancer();
+        }
+
         if(140 == kc) {
             zoom += 0.1;
             //System.out.println("Key pressed: zoom in="+zoom);
-        } 
-        
+        }
+
         if(139 == kc) {
             zoom -= 0.1;
             //System.out.println("Key pressed: zoom out");
-        } 
-        
+        }
+
         if(KeyEvent.VK_LEFT == kc) {
             view_roty -= 1;
             //System.out.println("Key pressed: view_roty="+view_roty);
-        } 
-        
+        }
+
         else if(KeyEvent.VK_RIGHT == kc) {
             view_roty += 1;
             //System.out.println("Key pressed: view_roty="+view_roty);
-        } 
-        
+        }
+
         else if(KeyEvent.VK_UP == kc) {
             view_rotx -= 1;
             //System.out.println("Key pressed: view_rotx="+view_rotx);
-        } 
-        
+        }
+
         else if(KeyEvent.VK_DOWN == kc) {
             view_rotx += 1;
             //System.out.println("Key pressed: view_rotx="+view_rotx);
         }
         else {
-        	//System.out.println(e.getKeyCode());
+            //System.out.println(e.getKeyCode());
         }
-        
+
         myGLEventListener.setView_rotx(view_rotx);
         myGLEventListener.setView_roty(view_roty);
         myGLEventListener.setScale(zoom);
     }
-	
-	public float getScale() {
-		return zoom;
-	}
-	
-	public void setScale(float scale) {
-		this.zoom = scale;
-	}
 
-  }
+    public float getScale() {
+        return zoom;
+    }
+
+    public void setScale(float scale) {
+        this.zoom = scale;
+    }
+
+}
